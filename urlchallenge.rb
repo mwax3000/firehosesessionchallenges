96da662d7d69e38a08ed72deb373a87c7ae1592e
VENANT MWAKIO <vmwakio@gmail.com>
2017-12-20 20:58:25 +0300

    refactor code

--- urlchallenge.rb
+++ urlchallenge.rb
@@ -2,17 +2,25 @@
 
 url = "https://www.google.com/search?q=kittens"
 def parse_url(url)
-  parsed_url ={}
-  parsed_url [:protocol] = url.split(':').first
-  parsed_url [:domain]=url.split('://')[1].split('/').first
-
-   
+  parsed_url = {}
+  parsed_url[:protocol] = parse_protocol(url)
+  parsed_url[:domain] = parse_domain(url)
+  parsed_url[:query] = parse_query(url)
+  
 
   parsed_url 
-  
-  
-  
-  
+end
+
+def parse_protocol(url)
+  url.split(':').first
+end
+
+def parse_domain(url)
+  url.split('://')[1].split('/').first
+end
+
+def parse_query(url)
+  url.split('?').last
 end
 
 puts parse_url(url)
